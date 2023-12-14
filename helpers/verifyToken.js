e3ad54dd45a8d8e4067807ebc1e67b61f90650be
Tom Pierce <tompierce4@gmail.com>
2023-12-14 14:41:00 +0000

    respond with error if JWToken is invalid instead of continuing as guest

--- helpers/verifyToken.js
+++ helpers/verifyToken.js
@@ -4,21 +4,22 @@ require("dotenv").config();
 // Verify token and set req.user
 const verifyToken = (req, res, next) => {
   const bearerHeader = req.headers["authorization"];
-  if (bearerHeader) {
-    const token = bearerHeader.split(" ")[1];
-    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-      if (err) {
-        req.user = {
-          guest: true,
-        };
-      } else {
-        req.user = decoded.user;
-      }
-      return next();
-    });
-  } else {
-    res.status(403).json({ message: "Forbidden" });
+  if (!bearerHeader) {
+    req.user = { guest: true };
+    return next();
   }
+  const token = bearerHeader.split(" ")[1];
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .json({ error: "Invalid token: Authentication failed" });
+    } else {
+      req.user = decoded.user;
+    }
+    return next();
+  });
+  return next();
 };
 
 module.exports = verifyToken;
